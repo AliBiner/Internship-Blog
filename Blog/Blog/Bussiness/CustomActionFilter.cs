401a02b6a3c2aca98b9ea142b52884acfbcf7788
Ali Biner <102030146+AliBiner@users.noreply.github.com>
2023-07-27 05:25:55 +0300

    Create CustomActionFilter

---
+++ Blog/Blog/Bussiness/CustomActionFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Bussiness
+{
+    public class CustomActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // Oturum süresi dolmuş mu kontrol ediyoruz.
+            if (filterContext.HttpContext.Session != null && filterContext.HttpContext.Session["Email"] == null)
+            {
+                // Kullanıcı oturumu yok veya süresi dolmuş, login sayfasına yönlendir.
+                filterContext.Result = new RedirectToRouteResult(new
+                    System.Web.Routing.RouteValueDictionary(new { controller = "Account", action = "Login" }));
+                return;
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
