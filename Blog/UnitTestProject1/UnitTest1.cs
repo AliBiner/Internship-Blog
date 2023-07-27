9ca2d30fe7e85bb092598a907a131a6f9e623ed9
Ali Biner <102030146+AliBiner@users.noreply.github.com>
2023-07-27 05:23:31 +0300

    Try Unit Test

---
+++ Blog/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Blog.Bussiness;
+using Blog.Key;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var password = "1234";
+            var hash = Methods.AesEncrypt(Keys.AesKey(),password);
+            var dehash = Methods.DecryptString(Keys.AesKey(), hash);
+
+            Assert.AreEqual(password,dehash);
+
+
+        }
+    }
+}
---
+++ Blog/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Blog.Bussiness;
+using Blog.Key;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var pass = "1234";
+            var dnm1 = Methods.Encrypt(pass,Keys.AesKey());
+            var dnm2 = Methods.Decrypt(dnm1, Keys.AesKey());
+
+            Assert.AreEqual(pass,dnm2);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var pass = "1234";
+            var dnm1 = Methods.Encrypt(pass, Keys.AesKey());
+            var dnm2 = Methods.Decrypt(dnm1, Keys.AesKey());
+
+            Assert.AreEqual(pass, dnm2);
+        }
+    }
+}
