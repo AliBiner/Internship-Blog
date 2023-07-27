dce88e044419013a93d89bd218d23ac15d9a0fe7
Ali Biner <102030146+AliBiner@users.noreply.github.com>
2023-07-27 05:41:42 +0300

    Add UnityConfig in Application_Start

--- Blog/Blog/Global.asax.cs
+++ Blog/Blog/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Blog.Models.Context;
 
 namespace Blog
 {
@@ -12,10 +13,12 @@ namespace Blog
     {
         protected void Application_Start()
         {
+            UnityConfig.RegisterComponents();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
         }
     }
 }
