90ef562d72985084aeeff76607538c4ba31b267f
Ali Biner <102030146+AliBiner@users.noreply.github.com>
2023-07-27 05:40:19 +0300

    Create ValidatePassword.js

---
+++ Blog/Blog/Scripts/MyScripts/ValidatePassword.js
@@ -0,0 +1,13 @@
+﻿var password = document.getElementById("PasswordHash"),
+    confirm_password = document.getElementById("passwordRepeat");
+
+function validatePassword() {
+    if (password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Şifre Eşleşmiyor");
+    } else {
+        confirm_password.setCustomValidity("");
+    }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
